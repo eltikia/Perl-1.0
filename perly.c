fe07105c64f090cac178b1b6b22d23e4965601cf
Maciej Matiaszowski <mmatiaszowski@genb.co>
2024-04-03 22:15:16 +0100

    Add the YYINT return type to the 'yylex' function.
    Co-authored-by: eltikia <maciej.matiaszowski.public@gmail.com>

--- perly.c
+++ perly.c
@@ -313,6 +313,7 @@ unsigned int cmdline = 65535;
 #define FUN3(f) return(yylval.ival = f,expectterm = FALSE,bufptr = s,(int)FUNC3)
 #define SFUN(f) return(yylval.ival=f,expectterm = FALSE,bufptr = s,(int)STABFUN)
 
+YYINT
 yylex()
 {
     register char *s = bufptr;
